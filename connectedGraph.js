3d001d986372149680f0e59cc878e2e69ed80087
Biki Deka <bikidekaantony@gmail.com>
2020-10-13 08:02:49 +0530

    just the graph comp is done but to do anything useful modification will be neccessary

---
+++ connectedGraph.js
@@ -0,0 +1,93 @@
+const template = document.createElement("template");
+
+template.innerHTML = `<svg width="700" height="700" class="svgEle"> </svg>`;
+
+class ConnectedGraph extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    d3.json("miserables.json").then((data) => {
+      console.log(data);
+      const width = 700;
+      const height = 700;
+
+      const links = data.links.map((d) => Object.create(d));
+      const nodes = data.nodes.map((d) => Object.create(d));
+
+      const simulation = d3
+        .forceSimulation(nodes)
+        .force(
+          "link",
+          d3.forceLink(links).id((d) => d.id)
+        )
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+      const svg = d3.select(this.shadowRoot).select("svg");
+
+      const link = svg
+        .append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+      const node = svg
+        .append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 5)
+        .attr("fill", "red")
+        .call(drag(simulation));
+
+      node.append("title").text((d) => d.id);
+
+      simulation.on("tick", () => {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+
+        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+
+        //invalidation.then(() => simulation.stop());
+      });
+    });
+  }
+
+  connectedCallback() {}
+}
+function drag(simulation) {
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
+
+  return d3
+    .drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended);
+}
+
+window.customElements.define("connected-graph", ConnectedGraph);
